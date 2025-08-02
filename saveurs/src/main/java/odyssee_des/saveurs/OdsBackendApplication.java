251956788bce352753891f7df3c4e6bab710820f
IrinaAndriakoto <119145568+IrinaAndriakoto@users.noreply.github.com>
2025-08-02 20:43:37 +0300

    base

---
+++ saveurs/src/main/java/odyssee_des/saveurs/OdsBackendApplication.java
@@ -0,0 +1,14 @@
+package odyssee_des.saveurs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OdsBackendApplication.class, args); 
+		
+	}
+
+}
---
+++ saveurs/src/test/java/odyssee_des/saveurs/OdsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package odyssee_des.saveurs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OdsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
